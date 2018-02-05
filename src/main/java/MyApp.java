4b7ff7337b38fe38eeb1deb4fcc3689d2399a700
vladimirsivanovs <vladimirs.ivanovs@ovoenergy.com>
2018-02-05 11:03:57 +0000

    initial commit

---
+++ src/main/java/MyApp.java
@@ -0,0 +1,80 @@
+import java.util.Stack;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        Bucket bucket = new Bucket();
+
+        Thread writerThread = new Thread(() -> {
+            while(true) {
+                synchronized (bucket) {
+                    if (bucket.isFull()) {
+                        System.out.println("Bucket is full. Waiting ...");
+
+                        try {
+                            bucket.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        int a = (int)(Math.random() * 10);
+                        System.out.println("Putting element into the bucket " + a);
+
+                        bucket.addElement(a);
+                    }
+                    bucket.notifyAll();
+
+                }
+            }
+
+        });
+
+        Thread readerThread = new Thread(() -> {
+            while (true) {
+                synchronized (bucket) {
+                    if (bucket.isEmpty()) {
+                        System.out.println("Bucket is empty. Waiting ...");
+                        try {
+                            bucket.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        int elem = bucket.readElement();
+                        System.out.println("Reading an element from the bucket " + elem);
+                    }
+
+                    bucket.notifyAll();
+
+                }
+            }
+
+        });
+
+
+        readerThread.start();
+        writerThread.start();
+
+    }
+
+}
+
+class Bucket {
+    private static Stack<Integer> stack = new Stack<>();
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public void addElement(int a) {
+        stack.push(a);
+    }
+
+    public int readElement() {
+        return stack.pop();
+    }
+
+    public boolean isFull() {
+        return stack.size() >= 10;
+    }
+}
